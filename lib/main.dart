64a872ba5527e6f4a9559fe6e619bffc347b52b7
Mohammed Al-Aishat <mohammadalaishat2005@gmail.com>
2025-08-06 02:58:38 +0000

    Removed .deleteEverything 🙂

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-import 'data/repo/local_data_repo.dart';
 import 'my_app.dart';
 
 void main() async {
@@ -16,8 +15,6 @@ void main() async {
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]);
-  final LocalDataRepo localDataRepo = getIt<LocalDataRepo>();
-  localDataRepo.deleteEverything();
   final routingUseCase = getIt<RoutingUseCase>();
   firstRoute = await routingUseCase.getFirstRoute();
   // usePathUrlStrategy();
